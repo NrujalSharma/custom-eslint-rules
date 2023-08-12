847180bf2490cf87d72c40aadf6901134d6ef8e2
NrujalSharma <snrujal@gmail.com>
2023-08-12 16:51:25 +0530

    feat: added eslint plugin config

---
+++ .eslint.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: ["custom-eslint-rules"], // Replace with the name of your actual plugin
+  rules: {
+    "custom-eslint-rules/require-min-cases-in-switch": ["error", 3], // Example rule configuration
+  },
+};
